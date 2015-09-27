b4b5a9fa53a6242962441fc38b06f49dd0b88a25
Dražen Lučanin <kermit666@gmail.com>
2015-09-27 20:26:09 +0200

    docker-compose Django quickstart

--- generators/app/templates/django/mysite/mysite/settings.py
+++ generators/app/templates/django/mysite/mysite/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
