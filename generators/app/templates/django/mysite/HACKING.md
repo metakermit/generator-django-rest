7fd51404360b333fb41e65ce3b4935adbd17c1ed
Dražen Lučanin <kermit666@gmail.com>
2017-03-19 14:27:05 +0100

    urls for whitenoise

--- generators/app/templates/django/mysite/HACKING.md
+++ generators/app/templates/django/mysite/HACKING.md
@@ -1,6 +1,6 @@
 # Development
 
-Setup your development environment:
+Setup your <%= project_name %> development environment:
 
     ./scripts/devsetup.sh
 
@@ -10,17 +10,27 @@ And start developing
 
 ## Upgrading the scaffolding
 
-The scaffolding for this project was built using Django-REST and a lot of the
-changes that benefit a wider range of projects (e.g. auth, user management,
-caching improvements etc.) could perhaps be contributed there. To update this
-project with the latest version of the Django-REST generator install node and
-get the generator-django-rest dependencies:
+The scaffolding for this Django project was built using
+[generator-django-rest][]. If you think of an improvement for your
+Django project and that could benefit a wider range of projects
+(e.g. auth, user management, caching improvements etc.), please consider
+[contributing back][generator-django-rest].
+
+To upgrade your project with the latest version of generator-django-rest
+install `node` and get these npm package:
 
     npm install -g yo generator-django-rest
     npm update -g yo generator-django-rest # maybe not necessary?
+
+If you use `yarn` instead do:
+
+    yarn global add yo generator-django-rest
+    yarn global upgrade yo generator-django-rest
 
 Then from within the root of your project (the path containing this file) run:
 
     yo django-rest <%= project_name %>
 
 And resolve any conflicts using the interactive queries.
+
+[generator-django-rest]: (http://github.com/metakermit/generator-django-rest)
--- generators/app/templates/django/mysite/mysite/urls.py
+++ generators/app/templates/django/mysite/mysite/urls.py
@@ -1,7 +1,7 @@
-"""<%= project_name %> URL Configuration
+"""posterbat URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -10,12 +10,36 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.conf import settings
+
+admin.autodiscover()
 
 urlpatterns = [
+
+    # static files (*.css, *.js, *.jpg etc.) served on /
+    url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',
+        RedirectView.as_view(url='/static/%(path)s', permanent=False)),
+
+    # other views still work too
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEPLOYMENT == 'dev':
+  urlpatterns = urlpatterns + static(
+      settings.MEDIA_URL,
+      document_root=settings.MEDIA_ROOT
+  )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
