2a2a2c801a7e38ec555f762dd759133b92be041c
Dražen Lučanin <kermit666@gmail.com>
2017-08-03 00:55:18 +0200

    use django-spa

--- generators/app/templates/django/mysite/mysite/middleware.py
+++ generators/app/templates/django/mysite/mysite/middleware.py
@@ -3,37 +3,6 @@ import re
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
-from whitenoise.middleware import WhiteNoiseMiddleware
-
-
-class IndexWhiteNoise(WhiteNoiseMiddleware):
-    """Adds support for serving index pages for directory paths."""
-
-    INDEX_NAME = 'static/index.html'
-
-    def update_files_dictionary(self, *args):
-        super(IndexWhiteNoise, self).update_files_dictionary(*args)
-        #import ipdb; ipdb.set_trace()
-        index_page_suffix = '/' + self.INDEX_NAME
-        index_name_length = len(self.INDEX_NAME)
-        directory_indexes = {}
-        for url, static_file in self.files.items():
-            if url.endswith(index_page_suffix):
-                # For each index file found, add a corresponding URL->content mapping
-                # for the file's parent directory, so that the index page is served for
-                # the bare directory URL ending in '/'.
-                parent_directory_url = url[:-index_name_length]
-                directory_indexes[parent_directory_url] = static_file
-        self.files.update(directory_indexes)
-
-    def find_file(self, url):
-        # In debug mode, find_file() is used to serve files directly from the filesystem
-        # instead of using the list in `self.files`, so we append the index filename so
-        # that will be served if present.
-        if url.endswith('/'):
-            url += self.INDEX_NAME
-        return super(IndexWhiteNoise, self).find_file(url)
-
 class RequireLoginMiddleware(object):
     """
     Middleware component that wraps the login_required decorator around
--- generators/app/templates/django/mysite/mysite/settings.py
+++ generators/app/templates/django/mysite/mysite/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    '<%= project_name %>.middleware.IndexWhiteNoise',
+    'spa.middleware.SPAMiddleware',
 ]
 
 ROOT_URLCONF = '<%= project_name %>.urls'
