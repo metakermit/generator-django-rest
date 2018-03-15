bb104100d71e0d32f54689ba6602fde91b5adf63
Jason Kraus <zbyte64@gmail.com>
2018-03-14 19:24:59 -0700

    opt-in to eventlet

--- generators/app/templates/django/mysite/mysite/gunicorn.conf.py
+++ generators/app/templates/django/mysite/mysite/gunicorn.conf.py
@@ -3,7 +3,7 @@ import os
 bind = '0.0.0.0:8000'
 workers = int(os.environ.get('WEB_WORKERS', '3'))
 timeout = int(os.environ.get('WEB_TIMEOUT', '30'))
-worker_class = os.environ.get('WEB_WORKER', 'eventlet')
+worker_class = os.environ.get('WEB_WORKER', 'sync') #recommended: eventlet
 reload = os.environ.get('DEBUG', '').upper() in ('1', 'TRUE')
 preload_app = False
 
