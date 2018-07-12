c5019ed60393c8f051f39b37c54a5e616f1e8946
Jason Kraus <zbyte64@gmail.com>
2018-07-12 13:48:26 -0700

    add snapshottesting to requirements, have Docker build install dev requirements

--- generators/app/templates/django/mysite/mysite/tests.py
+++ generators/app/templates/django/mysite/mysite/tests.py
@@ -1,9 +1,12 @@
 from snapshottest.django import TestCase
-from graphene.test import Client
+#from graphene.test import Client
+from django.test import Client
 
+
+client = Client()
 
 class APITestCase(TestCase):
-    def test_api_me(self):
-        """Testing the API for /me"""
-        my_api_response = api.client.get('/me')
+    def test_api_index(self):
+        """Testing the API for /graphql/"""
+        my_api_response = client.get('/graphql/')
         self.assertMatchSnapshot(my_api_response)
